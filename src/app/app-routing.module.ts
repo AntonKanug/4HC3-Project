a415148a57978cac39c4662c7fae56085cafec37
Anton Kanugalawattage <antondilon@gmail.com>
2022-11-07 18:59:05 -0500

    Create main components + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderManagerComponent } from './order-manager/order-manager.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { CardComponent } from './menu/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CheckoutComponent,
+    OrderManagerComponent,
+    CartComponent,
+    ConfirmationComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
