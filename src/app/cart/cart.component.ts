d7ca390160ebc701538f2ef67bf3a887a2c677a9
Usman Asad <asadu@mcmaster.ca>
2022-11-10 05:19:41 -0500

    added mini drop-down cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -14,7 +14,7 @@ export class CartComponent implements OnInit {
     {name: "Watermelon", price: 2.00, imageUrl: "../../assets/images/menuItems/watermelon.png", description: "A juicy red watermelon with seeds. A juicy red watermelon with seeds. A juicy red watermelon with seeds. ", calories: 75, count: 2, tags: [FoodTag.Vegetarian], isFavorite: true, isPopular:false, category:FoodCategory.Salad},
     {name: "Croissant", price: 4.32, imageUrl: "../../assets/images/menuItems/croissant.png", description: "A buttery croissant freshly made from paris. A buttery croissant freshly made from paris. A buttery croissant freshly made from paris. ", calories: 290, count: 8, tags: [FoodTag.Vegetarian], isPopular:true, isFavorite:true, category: FoodCategory.Salad}
   ]
-  deliveryChecked = true;
+  deliveryChecked = false;
   deliveryCost = 4.25;
 
   deliveryForm = new FormGroup({
