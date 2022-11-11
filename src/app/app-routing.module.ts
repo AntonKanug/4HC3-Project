311acce58cd917184013022e563f8a342bf4218a
Zhiming Zhao <zzmlegend@gmail.com>
2022-11-11 00:01:54 -0500

    Update app-routing.module.ts
    Added routing for confirmation page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { MenuComponent } from './menu/menu.component';
 import { BoardComponent } from './order-management/components/board/board/board.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 const routes: Routes = [
   { path: '', component: MenuComponent },
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
+  { path: 'confirmation', component: ConfirmationComponent},
   { path: 'manager', component: BoardComponent },
 ];
 
